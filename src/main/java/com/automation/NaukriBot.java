77199e797f2a758b88c2d3a8a5c389b1fbc87c96
dipankar_c <dc954725@gmail.com>
2026-02-08 20:58:57 +0530

    Fixed build faliur...

--- src/main/java/com/automation/NaukriBot.java
+++ src/main/java/com/automation/NaukriBot.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,7 +38,8 @@ public class NaukriBot {
         WebDriver driver = new ChromeDriver(options);
 
         // Additional JS injection to hide WebDriver flag
-        ((ChromeDriver) driver).executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
+        //((ChromeDriver) driver).executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
+        ((JavascriptExecutor) driver).executeScript("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})");
 
         try {
             System.out.println("🚀 Starting Naukri Bot...");
